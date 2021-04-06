b877e01e2e65581709c0aafa5fbab23ee803ba1a
Jason <jrosasrjf@gmail.com>
2021-04-05 22:56:38 -0500

    added a basic event listener for when the button is clicked

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,9 @@
+resumeButtonE1 = document.getElementById("resumeButton");
 testTextE1 = document.getElementById("testText");
 
+resumeButtonE1.addEventListener("click", resumeDownload);
 
-console.log(testTextE1);
+function resumeDownload() {
+    console.log("I have been clicked ");
+
+} 
